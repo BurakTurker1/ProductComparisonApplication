a7dae0f5c605d181098c5bda089651a3e18c99c4
Burak Türker <pythonu0@gmail.com>
2023-09-10 18:30:09 +0300

    products added to the dictionary

--- price.py
+++ price.py
@@ -4,25 +4,45 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.keys import Keys
-import time
-item_name = input("enter your item name: ")
-class MyTestCase(unittest.TestCase):
+
+# Ürün özelliklerini depolamak için bir sözlük oluşturun
+urun_ozellikleri = {}
+
+
+class Testim(unittest.TestCase):
 
     def setUp(self) -> None:
         self.driver = webdriver.Chrome()
 
-    def test_something(self):
+    def test_bir_sey(self):
         driver = self.driver
         driver.get("https://www.trendyol.com/")
-        search_bar = driver.find_element(By.CLASS_NAME, "V8wbcUhU")
-        search_bar.send_keys(item_name)
-        search_bar.send_keys(Keys.ENTER)
-        prices = driver.find_elements(By.CLASS_NAME, "prc-box-dscntd")
-        WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "prc-box-dscntd")))
-        for element in prices:
-            print(element.text)
-        time.sleep(25)
+        arama_cubugu = driver.find_element(By.CLASS_NAME, "V8wbcUhU")
 
+        urun_adi = input("Ürün adını giriniz: ")  # Kullanıcıdan ürün adını alın
+        arama_cubugu.send_keys(urun_adi)
+        arama_cubugu.send_keys(Keys.ENTER)
+        WebDriverWait(driver, 4).until(
+            expected_conditions.visibility_of_element_located((By.CLASS_NAME, "prc-box-dscntd"))
+        )
+
+        # Ürün fiyatlarını ve isimlerini al
+        fiyatlar = driver.find_elements(By.CLASS_NAME, "prc-box-dscntd")
+        urun_adlari = driver.find_elements(By.CLASS_NAME, "prdct-desc-cntnr-name.hasRatings")
+
+        # Verileri düzenle ve sözlüğe ekle
+        for index, (fiyat_elementi, ad_elementi) in enumerate(zip(fiyatlar, urun_adlari), start=1):
+            urun_ozellikleri[f'Ürün {index}'] = {
+                'İsim': ad_elementi.text,
+                'Fiyat': fiyat_elementi.text
+            }
+
+        # Ürün bilgilerini yazdır
+        for urun, detaylar in urun_ozellikleri.items():
+            print(f'{urun}:')
+            print(f'İsim: {detaylar["İsim"]}')
+            print(f'Fiyat: {detaylar["Fiyat"]}')
+            print('-' * 30)
 
     def tearDown(self) -> None:
         self.driver.close()
